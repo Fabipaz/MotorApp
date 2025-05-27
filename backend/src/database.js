d542009ff1a08645dbe755b3f23c58bdd0d23010
FabipazDev <fabipaz2803@gmail.com>
2025-05-26 20:38:02 -0500

    agregando correccion backend

--- backend/src/database.js
+++ backend/src/database.js
@@ -1,6 +1,8 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
+const isProduction = process.env.NODE_ENV === 'production';
+
 const sequelize = new Sequelize(
   process.env.DB_NAME,
   process.env.DB_USER,
@@ -10,12 +12,14 @@ const sequelize = new Sequelize(
     port: process.env.DB_PORT,
     dialect: process.env.DB_DIALECT || 'postgres',
     logging: false,
-    dialectOptions: {
-      ssl: {
-        require: true,
-        rejectUnauthorized: false // Importante para Render y la mayoría de servicios cloud
-      }
-    }
+    dialectOptions: isProduction
+      ? {
+          ssl: {
+            require: true,
+            rejectUnauthorized: false
+          }
+        }
+      : {}
   }
 );
 
